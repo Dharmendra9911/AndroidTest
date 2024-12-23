66550e964bfe9cf6aad85fdcdfc7e628be3fde68
DharmendraRughani9911 <165995047+DharmendraRughani9911@users.noreply.github.com>
2024-12-23 10:54:04 +0530

    FFFF

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.photo.androidtest"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.photo.androidtest"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
